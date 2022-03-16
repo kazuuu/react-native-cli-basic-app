d97abe04a3979bd528c2bb6f103df4c70df9f265
Marcello Kazuo <marcellokazuo@gmail.com>
2022-03-15 21:56:47 -0300

    Cards Style

--- BasicApp/App.js
+++ BasicApp/App.js
@@ -6,54 +6,29 @@ import {
   View,
 } from 'react-native';
 
-// *********************
-// Class Component
-// *********************  
-class App extends Component {
-  render() {
-    const msg = 'Estou aqui';
-    console.log('Class Component', msg);
+const App = () => {
+  const msg = 'Estou aqui';
+  console.log('Arrow Function Component', msg);
 
-    return (
-      <View style={styles.container}>
-        <Text>Hybrid Mobile App</Text>
-        <StatusBar style="auto" />
+  return (
+    <View style={styles.container}>
+      <Text>Hybrid Mobile App</Text>
+      <StatusBar style='auto' />
+      
+      <View style={styles.card}>
+        <Text>Este é o primeiro card</Text>
       </View>
-    );
-  }
-}
+      <View style={[styles.card, { backgroundColor: '#ee125a', }]}>
+        <Text>Este é o segundo card</Text>
+      </View>
+      <View style={[styles.card, { backgroundColor: '#91A2AD', }]}>
+        <Text>Este é o terceiro card</Text>
+      </View>
+    </View>
+  );
+};
+
 export default App;
-
-
-// *********************
-// Function Component
-// *********************
-// export default function App() {
-// const msg = 'Estou aqui';
-// console.log('Function Component', msg);
-//   return (
-//     <View style={styles.container}>
-//       <Text>Hybrid Mobile App</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-
-// *********************
-// Arrow Function Component
-// *********************
-// const App = () => {
-// const msg = 'Estou aqui';
-// console.log('Arrow Function Component', msg);
-//   return (
-//     <View style={styles.container}>
-//       <Text>Hybrid Mobile App</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// };
-// export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -61,5 +36,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 16,
+  },
+  card: {
+    width: '100%',
+    backgroundColor: '#ccc',
+    padding: 16,
+    borderRadius: 10,
+    marginBottom: 8,
   },
 });
