9a4b714abf2e35c0975829f3edbc00e1470fae5d
Marcello Kazuo <marcellokazuo@gmail.com>
2022-03-16 22:02:15 -0300

    debug

--- BasicApp/Card.js
+++ BasicApp/Card.js
@@ -4,6 +4,8 @@ import { StyleSheet, View, Text } from "react-native";
 const Card = (props) => {
 
     console.log("1");
+    let x;
+    // x.toString();
 
     return (
         <View style={[styles.card, {backgroundColor: props.backgroundColor ? props.backgroundColor : '#91A2AD'}]}>
